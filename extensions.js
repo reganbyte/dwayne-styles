98472c02ddced905b85890ed7791fefd93c0fa2d
reganbyte <info@reganbyte.com>
2025-02-24 09:21:19 +0000

    Update extensions.js
    Commenting out video ext to see if that resolves the chat issue

--- extensions.js
+++ extensions.js
@@ -101,31 +101,31 @@ export const MapExtension = {
   },
 }
 
-export const VideoExtension = {
-  name: 'Video',
-  type: 'response',
-  match: ({ trace }) =>
-    trace.type === 'ext_video' || trace.payload.name === 'ext_video',
-  render: ({ trace, element }) => {
-    const videoElement = document.createElement('video')
-    const { videoURL, autoplay, controls } = trace.payload
+// export const VideoExtension = {
+//   name: 'Video',
+//   type: 'response',
+//   match: ({ trace }) =>
+//     trace.type === 'ext_video' || trace.payload.name === 'ext_video',
+//   render: ({ trace, element }) => {
+//     const videoElement = document.createElement('video')
+//     const { videoURL, autoplay, controls } = trace.payload
 
-    videoElement.width = 240
-    videoElement.src = videoURL
+//     videoElement.width = 240
+//     videoElement.src = videoURL
 
-    if (autoplay) {
-      videoElement.setAttribute('autoplay', '')
-    }
-    if (controls) {
-      videoElement.setAttribute('controls', '')
-    }
+//     if (autoplay) {
+//       videoElement.setAttribute('autoplay', '')
+//     }
+//     if (controls) {
+//       videoElement.setAttribute('controls', '')
+//     }
 
-    videoElement.addEventListener('ended', function () {
-      window.voiceflow.chat.interact({ type: 'complete' })
-    })
-    element.appendChild(videoElement)
-  },
-}
+//     videoElement.addEventListener('ended', function () {
+//       window.voiceflow.chat.interact({ type: 'complete' })
+//     })
+//     element.appendChild(videoElement)
+//   },
+// }
 
 export const TimerExtension = {
   name: 'Timer',
